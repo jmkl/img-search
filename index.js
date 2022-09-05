c3f18d7e3857f9ae451554495740b413b41306db
jmkl <jimikill3d@gmail.com>
2022-09-05 19:46:12 +0700

    test

--- index.js
+++ index.js
@@ -18,7 +18,7 @@ app.get('/', function(req,res){
             res.send(JSON.stringify(result));
         })
     }else{
-        res.send(JSON.stringify([]))
+        res.send(JSON.stringify([{result:null}]))
     }
 
     
